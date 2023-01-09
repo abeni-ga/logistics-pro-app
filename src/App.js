5820bf1fb931af8c975583c1589330dff43bee00
Natnael Mekonnen <nhattymack204@gmail.com>
2023-01-09 13:24:16 +0300

    font change

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import { BrowserRouter } from "react-router-dom";
 import Routes from "./routes/index.routes";
+import { ThemeProvider, createTheme } from "@mui/material";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: "DM Sans",
+  },
+});
 
 function App() {
   return (
-    <div className="h-screen w-full bg-blue-50 overflow-hidden">
-      <BrowserRouter>
-        <Routes />
-      </BrowserRouter>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="h-screen w-full bg-blue-50 overflow-hidden">
+        <BrowserRouter>
+          <Routes />
+        </BrowserRouter>
+      </div>
+    </ThemeProvider>
   );
 }
 
